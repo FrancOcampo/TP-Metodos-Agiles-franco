ac91584b7de57f0183253b363a97642680c8a4dc
Aldana Ivón Herbel <aherbel@frsf.utn.edu.ar>
2025-05-24 11:49:45 -0300

    Conexión interfaz RegistroTitular con backend.

--- src/main/java/com/gestionlicencias/gestionlicenciasconducir/controller/TitularController.java
+++ src/main/java/com/gestionlicencias/gestionlicenciasconducir/controller/TitularController.java
@@ -1,6 +1,10 @@
 package com.gestionlicencias.gestionlicenciasconducir.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,8 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.gestionlicencias.gestionlicenciasconducir.service.TitularService;
 import com.gestionlicencias.gestionlicenciasconducir.dto.TitularRecord;
+import com.gestionlicencias.gestionlicenciasconducir.model.TipoDocumento;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -30,18 +38,20 @@ public class TitularController {
         this.service = service;
     }
 
+    /* 
     @Operation(summary = "Registrar un titular", 
                 description = "Registra un nuevo titular en la base de datos", 
                 responses = {
                     @ApiResponse(responseCode = "201", description = "Titular registrado correctamente"),
                     @ApiResponse(responseCode = "400", description = "Error al registrar el titular"),
                     @ApiResponse(responseCode = "500", description = "Error interno del servidor")
-                })
+                }) 
+    
     @PostMapping("/registrar")
     public ResponseEntity<Void> registrarTitular(@RequestBody @Valid TitularRecord titularRecord) {
         service.registrarTitular(titularRecord);
         return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+    } */
     
     //Endpoints para el front
 
@@ -50,6 +60,45 @@ public class TitularController {
 
     @GetMapping("/registroTitular")
     public String mostrarFormulario() {   return "registroTitular";    }
+
+    @PostMapping("/registrar")
+    public String registrarTitular(
+        @RequestParam TipoDocumento tipoDocumento,
+        @RequestParam String numeroDocumento,
+        @RequestParam String nombre,
+        @RequestParam String apellido,
+        @RequestParam String direccion,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaNacimiento,
+        @RequestParam String grupoSanguineo,
+        @RequestParam String factorRH,
+        @RequestParam(required = false, defaultValue = "false") boolean donante,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            java.sql.Date fechaNacimientoSql = java.sql.Date.valueOf(fechaNacimiento);
+
+            TitularRecord dto = new TitularRecord(
+                tipoDocumento,
+                numeroDocumento,
+                nombre,
+                apellido,
+                fechaNacimientoSql,
+                direccion,
+                grupoSanguineo,
+                factorRH,
+                donante
+            );
+            service.registrarTitular(dto);
+            redirectAttributes.addFlashAttribute("successMessage", "Titular registrado exitosamente.");
+
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Ocurrió un error al registrar el titular.");
+        }
+        return "redirect:/api/titulares/registroTitular";
+    }
 
     @GetMapping("/modificar")
     public String mostrarFormularioModificar() {   return "modificarTitular";    }
